4446e397f1d3add90ac9cac3c4bbab1b21789fe5
urajrishuu <urajrishuu@gmail.com>
2022-05-17 15:20:17 +0530

    Index

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('./route/route.js');
+const mongoose = require('mongoose');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+mongoose.connect("mongodb+srv://urajrishu:aUHDB96UyJaq9SB@cluster0.1wief.mongodb.net/urajrishu02", {useNewUrlParser:true})
+.then(() => console.log('Hey Yuvraj! Go Ahead MongoDB is connected'))
+.catch(err => console.log(err))
+app.use('/', route)
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log('Express is running on porn' + (process.env.PORT || 3000))
+});
