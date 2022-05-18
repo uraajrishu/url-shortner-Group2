b10bfb46a7b072d6a22759f145134753ce773dca
Nirajray <nirajray5555@gmail.com>
2022-05-18 14:58:04 +0530

    update

--- src/route/route.js
+++ src/route/route.js
@@ -9,5 +9,5 @@ router.get('/test-me', function (req, res) {
 
 
 router.post("/url/shorten", urlController.urlShorten)
-router.get("/code/:url", urlController.getCode)
+router.get("/:code", urlController.getCode)
 module.exports = router;
